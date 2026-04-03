7039c31e0f60a912e9737ed5f57e9c19d227f4d6
samclark117117117-cmd <samclark117117117@gmail.com>
2026-04-03 21:26:39 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import App from './glowup-app';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<React.StrictMode><App /></React.StrictMode>);
